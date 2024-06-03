8edfee39d708d19957f5b33d00b7b26d72085250
Juan carlos Suazo dirocie <juancarlossuazodirocie@Juans-MacBook-Pro.local>
2024-06-03 14:44:32 -0400

    added logic for game over and draw

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,27 @@ import "./App.css";
 import useGame from "./hooks/useGame";
 
 function App() {
-  const { board, playerAction } = useGame();
+  const { board, playerAction, winner, isGameOver } = useGame();
   return (
     <div className="app">
       <h1>Tic Tac Toe</h1>
-      {board.map((currentBoard, currentBoardIndex) => (
-        <div className="board" key={currentBoardIndex}>
-          {currentBoard.map((cell, cellIndex) => (
-            <div key={cellIndex} className="cell" onClick={() => playerAction(currentBoardIndex, cellIndex)}>{cell}</div>
-          ))}
-        </div>
-      ))}
+      
+      {isGameOver ? (
+        <>
+        <h2>Game Over</h2>
+        {winner ? (<p>The winner is <strong>{winner}</strong></p>) : (<p>It was a draw</p>)}
+        
+        <button onClick={() => window.location.reload()}>Play again</button>
+        </>
+      ):(
+        board.map((currentBoard, currentBoardIndex) => (
+          <div className="board" key={currentBoardIndex}>
+            {currentBoard.map((cell, cellIndex) => (
+              <div key={cellIndex} className="cell" onClick={() => playerAction(currentBoardIndex, cellIndex)}>{cell}</div>
+            ))}
+          </div>
+        ))
+      ) }
     </div>
   );
 }
--- src/hooks/useGame.tsx
+++ src/hooks/useGame.tsx
@@ -2,20 +2,40 @@ import { useState } from "react";
 
 function useGame() {
   const [isGameOver, setGame] = useState(false)
-  const [winner, setWinner] = useState(null)
+  const [winner, setWinner] = useState<null | string>(null)
   const [currentPlayer, setCurrentPlayer] = useState('X')
+  const [attempsLeft, setAttempsLeft] = useState(8);
   const [board, setBoard] = useState([
     ['', '', ''],
     ['', '', ''],
     ['', '', ''],
   ])
-  function playerAction(currentBoardIndex: number, cellIndex: number) {
-    if (isGameOver) {
+
+  function checkWinner(cellIndex: number, currentBoardRowIndex: number, winner: string) {
+    board[currentBoardRowIndex][cellIndex] = winner;
+    setBoard(board)
+    setAttempsLeft(attempsLeft => attempsLeft - 1);
+    const condition1 = board[currentBoardRowIndex][0] === winner && board[currentBoardRowIndex][1] === winner && board[currentBoardRowIndex][2] === winner
+    const condition2 = board[0][cellIndex] === winner && board[1][cellIndex] === winner && board[2][cellIndex] === winner
+    const condition3 = board[0][0] === winner && board[1][1] === winner && board[2][2] === winner
+    const condition4 = board[0][2] === winner && board[1][1] === winner && board[2][0] === winner
+    
+    if ( condition1 || condition2 || condition3 || condition4){
+      setGame(true);
+      setWinner(winner);
+      return;
+    } else if (attempsLeft <= 0) {
+      setGame(true);
+      setWinner(null);
       return;
     }
-    board[currentBoardIndex][cellIndex] = currentPlayer
+  }
+  function playerAction(currentBoardRowIndex: number, cellIndex: number) {
+    if (isGameOver || board[currentBoardRowIndex][cellIndex] !== '') {
+      return;
+    }
+    checkWinner(cellIndex, currentBoardRowIndex, currentPlayer);
 
-    setBoard(board)
     if (currentPlayer === 'X') {
       setCurrentPlayer('O');
     } else {
